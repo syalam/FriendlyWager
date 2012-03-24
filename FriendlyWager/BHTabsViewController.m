341229e98160dffd093c0746bfe1fd7ac03b4700
Reyaad Sidique <reyaad@tappforce.com>
2012-03-24 00:21:38 -0700

    fixed tabs view
    Signed-off-by: Reyaad Sidique <reyaad@tappforce.com>

--- FriendlyWager/BHTabsViewController.m
+++ FriendlyWager/BHTabsViewController.m
@@ -130,7 +130,8 @@ enum { kTagTabBase = 100 };
     
     // The view that contains the tab views is located across the top.
     
-    CGRect tabsViewFrame = CGRectMake(0, 30, frame.size.width, self.style.tabsViewHeight);
+    //CGRect tabsViewFrame = CGRectMake(0, 20, frame.size.width, self.style.tabsViewHeight);
+    CGRect tabsViewFrame = CGRectMake(0, 30, frame.size.width, 75);
     self.tabsContainerView = [[[BHTabsView alloc] initWithFrame:tabsViewFrame] autorelease];
     self.tabsContainerView.backgroundColor = [UIColor clearColor];
     self.tabsContainerView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
@@ -172,6 +173,7 @@ enum { kTagTabBase = 100 };
     self.tabsContainerView.tabViews = allTabViews;
     
     CGRect footerFrame = CGRectMake(0, tabsViewFrame.size.height - self.style.tabBarHeight - self.style.shadowRadius, tabsViewFrame.size.width, self.style.tabBarHeight + self.style.shadowRadius);
+    
 
     self.footerView = [[[BHTabsFooterView alloc] initWithFrame:footerFrame] autorelease];
     self.footerView.backgroundColor = [UIColor clearColor];
